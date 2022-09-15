a77226e87cc958da65775f2bf84b00d57962fa7d
Rafael Guzman <rega1237@gmail.com>
2022-09-15 18:20:19 -0400

    add some routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users do
+    resources :reservations
+  end
+
+  resources :stadiums do
+    resources :games
+  end
 end
