bcf4e8d59befecd87b38f55a958729c396088cbd
Rafael Guzman <rega1237@gmail.com>
2022-09-16 12:11:32 -0400

    add more data to the response

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -5,7 +5,7 @@ class AuthenticationController < ApplicationController
      @user = User.find_by_username(params[:username])
      if @user
         token = jwt_encode(user_id: @user.id)
-        render json: { token: token }, status: :ok
+        render json: { token: token, username: @user.username, user_id: @user.id }, status: :ok
      else
         render json: { error: 'Unauthorized' }, status: :unauthorized
      end
