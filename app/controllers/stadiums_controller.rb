5f80a6aa606be5d624fd060edf33ca46283fb7e8
Rafael Guzman <rega1237@gmail.com>
2022-09-16 15:14:14 -0400

    show method for stadium controller

--- app/controllers/stadiums_controller.rb
+++ app/controllers/stadiums_controller.rb
@@ -3,4 +3,9 @@ class StadiumsController < ApplicationController
     @stadiums = Stadium.all
     render json: @stadiums, status: :ok
   end
+
+  def show
+    @stadium = Stadium.find(params[:id])
+    render json: @stadium, status: :ok
+  end
 end
