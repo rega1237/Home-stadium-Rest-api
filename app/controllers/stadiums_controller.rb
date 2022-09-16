73ac8d899424c7f87b8e5e3c6296a0a2c210b7a0
Rafael Guzman <rega1237@gmail.com>
2022-09-16 15:19:56 -0400

    create method for Stadium controller

--- app/controllers/stadiums_controller.rb
+++ app/controllers/stadiums_controller.rb
@@ -8,4 +8,19 @@ class StadiumsController < ApplicationController
     @stadium = Stadium.find(params[:id])
     render json: @stadium, status: :ok
   end
+
+  def create
+    @stadium = Stadium.new(stadium_params)
+    if @stadium.save
+      render json: @stadium, status: :created
+    else
+      render json: @stadium.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def stadium_params
+    params.require(:stadium).permit(:name, :county, :seats)
+  end
 end
