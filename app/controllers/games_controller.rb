9500454947db12b05ee41424a787f99d418ec12f
Dangelo Arrivillaga <98621809+Dangelo-JAN@users.noreply.github.com>
2022-09-16 19:43:12 -0400

    Create games create method

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,17 @@
 class GamesController < ApplicationController
+  def show
+    @game = Game.find(params[:id])
+    render json: {all_data: { games: @game, team_a: @game.team_a.name, team_b: @game.team_b.name }}, status: :ok
+  end
 
   def create
     @game = Game.new(games_params)
+    @game.stadium_id = params[:stadium_id]
+    @game.team_a = Team.find(params[:game][:team_a])
+    @game.team_b = Team.find(params[:game][:team_b])
     if @game.save
       render json: @game, status: :ok
     else
-      @user = User.find(params[:user_id])
       render json: @game.errors, status: :unprocessable_entity
     end
   end
@@ -17,6 +23,6 @@ class GamesController < ApplicationController
   private
 
   def games_params
-    params.require(:game).permit(:team_a, :team_b, :date, :stadium_id)
+    params.require(:game).permit(:date)
   end
 end
