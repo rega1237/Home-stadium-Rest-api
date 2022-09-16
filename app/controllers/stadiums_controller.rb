06b25acd8c0a5b4a013ed87a1ac4deadb744624d
Rafael Guzman <rega1237@gmail.com>
2022-09-16 15:25:35 -0400

    destroy method and add set stadium method to prevent the DRY issue

--- app/controllers/stadiums_controller.rb
+++ app/controllers/stadiums_controller.rb
@@ -1,11 +1,12 @@
 class StadiumsController < ApplicationController
+  before_action :set_stadium, only: [:show, :destroy]
+
   def index
     @stadiums = Stadium.all
     render json: @stadiums, status: :ok
   end
 
   def show
-    @stadium = Stadium.find(params[:id])
     render json: @stadium, status: :ok
   end
 
@@ -18,9 +19,18 @@ class StadiumsController < ApplicationController
     end
   end
 
+  def destroy
+    @stadium.destroy
+    render json: @stadium, status: :ok
+  end 
+
   private
 
   def stadium_params
     params.require(:stadium).permit(:name, :county, :seats)
+  end
+
+  def set_stadium
+    @stadium = Stadium.find(params[:id])
   end
 end
