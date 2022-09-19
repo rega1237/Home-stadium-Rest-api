7154e6614d967c6b32e9b23701bb9e45604110c8
Rafael Guzman <rega1237@gmail.com>
2022-09-19 17:26:20 -0400

    create show method for reservations controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,6 +1,18 @@
 class ReservationsController < ApplicationController
+  before_action :set_reservation, only: %i[show destroy]
+
   def index
     @reservations = Reservation.where(user_id: @current_user_id)
     render json: @reservations, each_serializer: ReservationsSerializer
+  end
+  
+  def show
+    render json: @reservation, serializer: ReservationsSerializer
+  end
+
+  private
+
+  def set_reservation
+    @reservation = Reservation.find(params[:id])
   end
 end
