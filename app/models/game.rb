77b88b74041f19341458c355dc4a648e47300e3d
Dangelo Arrivillaga <98621809+Dangelo-JAN@users.noreply.github.com>
2022-09-19 17:14:51 -0400

    Update validations models

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,7 @@ class Game < ApplicationRecord
   belongs_to :stadium
   has_many :game_teams
   has_many :teams, through: :game_teams
+
+  validates :stadium, :date, :available_seats, presence: true
+  validates :available_seats, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
