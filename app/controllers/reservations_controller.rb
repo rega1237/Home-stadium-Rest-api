938c8d347330ed7c2f3380cd20e0a3551def8af5
Rafael Guzman <rega1237@gmail.com>
2022-09-19 17:26:55 -0400

    Add create method to reservations controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -10,7 +10,27 @@ class ReservationsController < ApplicationController
     render json: @reservation, serializer: ReservationsSerializer
   end
 
+  def create
+    @game = Game.find(params[:reservation][:game_id])
+    @reservation = Reservation.new(reservation_params)
+    @reservation = @game.reservations.create(reservation_params)
+    @reservation.user = @current_user_id
+    if @reservation.save
+      render json: @reservation, status: :created
+    else
+      render json: @reservation.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    #implement
+  end
+
   private
+
+  def reservation_params
+    params.require(:reservation).permit(:reserved_seats)
+  end
 
   def set_reservation
     @reservation = Reservation.find(params[:id])
