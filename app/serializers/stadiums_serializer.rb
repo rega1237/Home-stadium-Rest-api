8bba5db9e755a12da40aec6abfec04ffe12c6002
Rafael Guzman <rega1237@gmail.com>
2022-09-20 11:43:27 -0400

    map the game into stadium to not show the games with available seats = 0

--- app/serializers/stadiums_serializer.rb
+++ app/serializers/stadiums_serializer.rb
@@ -2,16 +2,19 @@ class StadiumsSerializer < ActiveModel::Serializer
   attributes :id, :name, :country, :seats, :coming_games
   def coming_games
     object.games.map do |game|
-      {
-        teams: game.teams.map do |team|
-          {
-            name: team.name,
-            flag: team.flag
-          }
-        end,
-        date: game.date,
-        aviable_seats: game.available_seats
-      }
+      if game.available_seats > 0
+        {
+          teams: game.teams.map do |team|
+            {
+              name: team.name,
+              flag: team.flag
+            }
+          end,
+          date: game.date,
+          aviable_seats: game.available_seats
+        }
+      end
     end
   end
 end
+
