fb319142803678c9e93c7308087431723d7f1589
Dangelo Arrivillaga <98621809+Dangelo-JAN@users.noreply.github.com>
2022-09-20 11:26:28 -0400

    Minor updates

--- app/models/game.rb
+++ app/models/game.rb
@@ -6,5 +6,6 @@ class Game < ApplicationRecord
   belongs_to :stadium
 
   validates :stadium, :date, :available_seats, presence: true
+  validates :date, uniqueness: true
   validates :available_seats, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  include Warden::Test::Helpers
+
+  before :each do
+    @user = User.create(
+      username: 'username',
+    )
+
+    login_as(@user, scope: :user)
+  end
+
+  describe 'User created' do
+    it 'should return a success response' do
+      get group_expenses_path(@group)
+      expect(response).to be_successful
+    end
+
+    it 'should return a 200 response' do
+      get group_expenses_path(@group)
+      expect(response).to have_http_status '200'
+    end
+
+    it 'should return a list of expenses' do
+      get group_expenses_path(@group)
+      expect(response.body).to include @expense.name
+    end
   end
 end
