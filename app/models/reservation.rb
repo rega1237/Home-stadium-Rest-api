ff10bb60e0852775e788638b6f7e9f30abc49433
Rafael Guzman <rega1237@gmail.com>
2022-09-20 11:41:17 -0400

    add method to reservation model to subtract  the number of reserved seats to the game

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,13 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :game
+
+  after_save :update_avilable_seats
+
+  private
+
+  def update_avilable_seats
+    game = Game.find(self.game_id)
+    game.update(available_seats: game.available_seats - self.reserved_seats)
+  end
 end
