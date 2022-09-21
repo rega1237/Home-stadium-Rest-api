c91ec550beb6d8a52b456ccf78c5b59cf0ed712b
Dangelo Arrivillaga <98621809+Dangelo-JAN@users.noreply.github.com>
2022-09-21 11:52:10 -0400

    update game spec

--- spec/requests/games_spec.rb
+++ spec/requests/games_spec.rb
@@ -1,38 +1,43 @@
 require 'rails_helper'
 
-RSpec.describe 'Games', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/games/index'
-      expect(response).to have_http_status(:success)
-    end
+RSpec.describe GamesController, type: :request do
+  include Warden::Test::Helpers
+
+  before :each do
+    @user = User.create(
+      username: 'username'
+    )
+
+    login_as(@user, scope: :user)
+
+    @stadium = Stadium.create(
+      name: 'Stadium`s name',
+      country: 'Stadium`s country',
+      seats: 10,
+      photo: 'Stadium`s photo'
+    )
+
+    @game = Game.create(
+      date: '01-01-1011',
+      stadium_id: @stadium.id,
+      available_seats: 5
+    )
   end
 
-  describe 'GET /show' do
-    it 'returns http success' do
-      get '/games/show'
-      expect(response).to have_http_status(:success)
+  describe 'GET #show' do
+    it 'should return a success response' do
+      get stadium_game_path(@game)
+      expect(response).to be_successful
     end
-  end
 
-  describe 'GET /new' do
-    it 'returns http success' do
-      get '/games/new'
-      expect(response).to have_http_status(:success)
+    it 'should return a 200 response' do
+      get stadium_game_path(@stadium)
+      expect(response).to have_http_status 'ok'
     end
-  end
 
-  describe 'GET /create' do
-    it 'returns http success' do
-      get '/games/create'
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe 'GET /delete' do
-    it 'returns http success' do
-      get '/games/delete'
-      expect(response).to have_http_status(:success)
+    it 'should return a list of games' do
+      get stadium_game_path(@stadium)
+      expect(response.body).to include @game.date
     end
   end
 end
