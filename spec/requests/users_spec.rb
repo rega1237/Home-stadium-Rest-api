df9ac046f0e220ef60d1ef974374a1b56677425c
Rafael Guzman <rega1237@gmail.com>
2022-09-21 12:46:42 -0400

    users request response success

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,33 +3,18 @@ require 'rails_helper'
 RSpec.describe 'Users', type: :request do
   include Warden::Test::Helpers
 
-  before :each do
-    @user = User.create(
-      username: 'username',
-    )
-
-    login_as(@user, scope: :user)
+  before :all do
+    @user = User.order(:id).first
+    post auth_login_path, headers: { 'Content-Type': 'application/json' }, params: { username:@user.username }.to_json  
+    # @token = JSON.parse(response.body)['token']
   end
 
   describe 'action users#current' do
-    before(:each) do
-      post 'http://localhost:3000/auth/login', params: { username: 'username' }
-    end
-
     context 'when logged in' do
       it 'returns http_success' do
         expect(response).to have_http_status(:success)
       end
 
-      # it 'header "Content-Type" contains application/json' do
-      #   expect(response.header['Content-Type']).to include('application/json')
-      # end
-
-      # it 'returns current user info' do
-      #   @body = JSON.parse(response.body)
-      #   expect(@body['name']).to eq 'Roberto'
-      #   expect(@body['email']).to eq 'roberto@mail.com'
-      # end
     end
   end
 end
