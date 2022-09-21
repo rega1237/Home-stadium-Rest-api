729a7f3ca65236e5ac08c2beb6d204143e9edccd
Dangelo Arrivillaga <98621809+Dangelo-JAN@users.noreply.github.com>
2022-09-21 15:49:19 -0400

    Stadiums spec working

--- spec/requests/stadiums_spec.rb
+++ spec/requests/stadiums_spec.rb
@@ -1,15 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe 'Stadiums', type: :request do
-      
-      let!(:user) { User.order(:id).first }
+  include Warden::Test::Helpers
 
-    describe 'For allowed user' do
-        before do
-          login(user)
-          get :show, params: { id: 1 }
-        end
-        
-        it { expect(response).to have_http_status(:ok) }
+  before :all do
+    @user = User.order(:id).first
+    @stadium = Stadium.order(:id).first
+    post auth_login_path, headers: { 'Content-Type': 'application/json' }, params: { username:@user.username }.to_json  
+    @token = JSON.parse(response.body)['token']
+  end
+
+  describe 'Stadiums`s Actions' do
+    it 'Return Index success' do
+      get stadiums_path, headers: { 'Content-Type': 'application/json', 'Authorization': @token }, params: { username:@user.username }.to_json
+      expect(response).to have_http_status(:success)
     end
+
+    it 'Return Show success' do
+      get stadium_path(@stadium.id), headers: { 'Content-Type': 'application/json', 'Authorization': @token }, params: { username:@user.username }.to_json
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'Return Create success' do
+      post stadiums_path, headers: { 'Content-Type': 'application/json', 'Authorization': @token }, params: { 
+        name: 'Stadium`s name',
+        country: 'Stadium`s country',
+        seats: 10,
+        photo: 'Stadium`s photo'
+      }.to_json
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'Return Delete success' do
+      delete stadium_path(@stadium.id), headers: { 'Content-Type': 'application/json', 'Authorization': @token }, params: { username:@user.username }.to_json
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
