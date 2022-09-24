73eb059223420d4e6bce10488fefdbc61dd3c5c3
Dangelo Arrivillaga <98621809+Dangelo-JAN@users.noreply.github.com>
2022-09-23 22:29:12 -0400

    Documentation almost done

--- spec/requests/api/stadiums_spec.rb
+++ spec/requests/api/stadiums_spec.rb
@@ -1,6 +1,8 @@
 require 'swagger_helper'
 
 RSpec.describe 'stadiums', type: :request do
+
+  # ----- Documentation to get user token ----- #
   path '/auth/login' do
     post ('User token') do
       tags 'User'
@@ -27,14 +29,32 @@ RSpec.describe 'stadiums', type: :request do
     end
   end
 
+  # ----- Documentation to Reservations ----- #
+  path '/users/{id}/reservations' do
+    get('list reservations') do
+      tags 'Reservations'
+      security [ token: [] ]
+      consumes 'application/json'
+      produces 'application/json'
+      parameter in: :path, type: :integer, name: 'id'
+      
+      response(200, 'successful') do
+        run_test!
+      end
+    end
+  end
+
+
+
+  # ----- Documentation to Stadiums ----- #
   path '/stadiums' do
     get('list staduims') do
-      tags 'Stadium'
+      tags 'Stadiums'
+      security [ token: [] ]
       consumes 'application/json'
       produces 'application/json'
 
       response(200, 'successful') do
-        security [ token: [] ]
         schema type: :object,
                properties: {
                  name: { type: :string },
@@ -45,72 +65,114 @@ RSpec.describe 'stadiums', type: :request do
         run_test!
       end
     end
-  end  
 
-  #   post('create stadium') do
-  #     tags 'Stadiums'
-  #     response(200, 'successful') do
-  #       consumes 'application/json'
-  #       parameter name: :stadium, in: :body, schema: {
-  #         type: :object,
-  #         properties: {
-  #           name: { type: :string },
-  #           country: { type: :string },
-  #           seats: { type: :integer },
-  #           photo: { Type: :string }
-  #         },
-  #         required: %w[name country seats photo]
-  #       }
-  #       run_test!
-  #     end
-  #   end
-  # end
+    post('create stadium') do
+      tags 'Stadiums'
+      security [ token: [] ]
+      consumes 'application/json'
+      produces 'application/json'
 
-  # path '/stadiums/{id}' do
-  #   get 'Show stadium' do
-  #     tags 'Stadiums'
-  #     produces 'application/json'
-  #     consumes 'application/json'
+      parameter name: :stadium, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string },
+          country: { type: :string },
+          seats: { type: :integer },
+          photo: { Type: :string }
+        },
+        required: %w[name country seats photo]
+      }
 
-  #     parameter name: 'id', in: :path, type: :integer
-  #     security [ api_key: [] ]
+      response(200, 'successful') do
+        parameter name: :stadium, in: :body, schema: {
+          type: :object,
+          properties: {
+            name: { type: :string },
+            country: { type: :string },
+            seats: { type: :integer },
+            photo: { Type: :string }
+          },
+          required: %w[name country seats photo]
+        }
+        run_test!
+      end
+    end
+  end
 
-  #     parameter({
-  #       :in => :header,
-  #       :type => :string,
-  #       :name => :Authorization,
-  #       :required => true,
-  #       :description => 'Client token'
-  #     })
+  path '/stadiums/{id}' do
+    get 'Show stadium' do
+      tags 'Stadiums'
+      security [ token: [] ]
+      produces 'application/json'
+      parameter name: 'id', in: :path, type: :integer
+      response '200', 'stadium found' do
+        schema type: :object,
+               properties: {
+                 id: { type: :integer },
+                 name: { type: :string },
+                 country: { type: :string },
+                 seats: { type: :integer },
+                 photo: { Type: :string },
+                 coming_games: { type: :array,
+                   items: { type: :object,
+                     properties: {
+                       game_id: { type: :integer },
+                       teams: { type: :array,
+                         items: { type: :object,
+                           properties: {
+                             name: { type: :string },
+                             flag: { type: :string }
+                           }
+                         }
+                       },
+                       date: { type: :string },
+                       available_seats: { type: :integer }
+                     }
+                   }
+                 }
+               }
+        run_test!
+      end
 
-  #     response '200', 'stadium found' do
-  #       let(:Authorization) { 'Bearer ' + generate_token }
-  #       schema type: :object,
-  #              properties: {
-  #                name: { type: :string },
-  #                country: { type: :string },
-  #                seats: { type: :integer },
-  #                photo: { Type: :string },
-  #              },
-  #              required: %w[name country seats photo]
+      response '404', 'Stadium Not FOUND' do
+        run_test!
+      end
+    end
 
-  #       let(:id) { Blog.create(title: 'foo', content: 'bar').id }
-  #       run_test!
-  #     end
+    delete('delete stadium') do
+      tags 'Stadiums'
+      security [ token: [] ]
+      parameter name: 'id', in: :path, type: :integer
+      response(200, 'successful') do
+        run_test!
+      end
+    end
+  end
 
-  #     response '404', 'blog not found' do
-  #       let(:id) { 'invalid' }
-  #       run_test!
-  #     end
-  #   end
+  # ----- Documentation to Games ----- #
+  path '/stadiums/{stadium_id}/games/{id}' do
+    get 'Show Game' do
+      tags 'Games'
+      security [ token: [] ]
+      produces 'application/json'
+      parameter in: :path, type: :integer, name: 'stadium_id'
+      parameter in: :path, type: :integer, name: 'id'
 
-  #   delete('delete stadium') do
-  #     tags 'Stadiums'
-  #     response(200, 'successful') do
-  #       let(:id) { '123' }
+      response(200, 'successful') do
+        run_test!
+      end
+    end
 
-  #       run_test!
-  #     end
-  #   end
-  # end
+    delete 'Show Game' do
+      tags 'Games'
+      security [ token: [] ]
+      produces 'application/json'
+      parameter in: :path, type: :integer, name: 'stadium_id'
+      parameter in: :path, type: :integer, name: 'id'
+
+      response(200, 'successful') do
+        run_test!
+      end
+    end
+  end
 end
