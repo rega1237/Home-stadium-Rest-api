a8dfc71fb347ffb317c4ce48626dc803bc759dbc
Rafael Guzman <rega1237@gmail.com>
2022-09-27 12:42:28 -0400

    add exception to routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,12 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   post '/auth/login', to: 'authentication#login'
-  resources :users do
-    resources :reservations
+  resources :users, only: [:create] do
+    resources :reservations, only: %i[index create destroy show] 
   end
 
-  resources :stadiums do
-    resources :games
+  resources :stadiums, only: %i[index create show destroy] do
+    resources :games, only: %i[create show destroy]
   end
 
   resources :teams, only: [:index] do
