513f18ee7112558a97d3037ab85bad66ee3e6914
Rafael Guzman <rega1237@gmail.com>
2022-09-28 13:31:14 -0400

    change route to the swagger

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -25,10 +25,10 @@ RSpec.configure do |config|
       paths: {},
       servers: [
         {
-          url: 'http://localhost:3000',
+          url: 'https://stadium-api-rails.herokuapp.com',
           variables: {
             defaultHost: {
-              default: 'www.ruta-deploy.com'
+              default: 'https://stadium-api-rails.herokuapp.com/'
             }
           }
         }
